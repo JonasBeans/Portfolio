850ab58410076f3e15a110b4569917f0723e9380
JonasBeans <boon.jonas.git@outlook.com>
2024-04-07 23:04:42 +0200

    #1: Responsive design

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -12,10 +12,42 @@ export class ProjectsComponent {
     public currentMarkdown: string = "../../assets/posts/LinodePortfolioWebsite.md";
     public posts?: String[];
 
+    public startIndex = 0;
+    public endIndex = 5;
+    public enablePosts: boolean = false;
+
     public constructor(private blogService: BlogService) { };
 
     ngOnInit(){
         this.blogService.getPostTitles().subscribe((result) => this.posts = result);
+    }
+
+    receivePageContent(pageContent: BlogPost) {
+        const decoder = new TextDecoder('utf-8');
+        this.currentMarkdown = decoder
+            .decode(Uint8Array.from(atob(pageContent.content), c => c.charCodeAt(0)).buffer);
+    }
+    
+    switchMenu() {
+        this.enablePosts = !this.enablePosts;
+    }
+
+    getPosts(): String[]{
+        return this.posts == undefined ?  
+            [] : 
+            this.posts.slice(this.startIndex, this.endIndex);
+    }
+
+    getNext() {
+        if(this.posts != undefined && this.endIndex + 1 <= this.posts.length ){
+            this.startIndex += 5;
+            this.endIndex += 5;
+        }
+    }
+
+    getPrevious() {
+        (this.startIndex - 5) <= 0 ?  this.startIndex = 0 : this.startIndex -= 5;
+        (this.endIndex - 10) <= 0 ? this.endIndex = 5 : this.endIndex -= 5 ;
     }
 
 }
